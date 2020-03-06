ad4608c1aa5a5dc3f93e0a529ce3318b03480dea
vince19972 <vince19972@gmail.com>
2020-03-06 15:57:47 -0500

    [header] add hover style, logo asset

--- phps/_header.php
+++ phps/_header.php
@@ -1,3 +1,18 @@
 <header class="header" role="banner">
-	<h1>test</h1>
+	<nav class="nav">
+		<button class="nav__btn -left -chevron">
+			<div class="nav__btn-chevron"></div>
+		</button>
+		<button class="nav__btn -center -ham">
+			<div class="nav__btn-ham"></div>
+		</button>
+		<button class="nav__btn -right -chevron">
+			<div class="nav__btn-chevron"></div>
+		</button>
+	</nav>
+	<div class="header__logo">
+		<a href="#" class="header__logo-link">
+			<img src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/logo.png" alt="logo" class="header__logo-img">
+		</a>
+	</div>
 </header>
