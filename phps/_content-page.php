58643e7907779c41dcb26c05db2109fed11bf31f
noisyneuron <hello@playdo.io>
2020-03-20 12:55:21 -0400

    [structure] added partials for home/post content and conditional rendering

---
+++ phps/_content-page.php
@@ -0,0 +1,1 @@
+<?php echo "~~~~~~~~~~ NORMAL PAGE (ABOUT ETC) ~~~~~~~~~~" ?>
--- phps/_contentArea.php
+++ phps/_contentArea.php
@@ -2,4 +2,10 @@
 
 	<?php get_template_part( './phps/_menu' ); ?>
 
+	<?php
+		if(is_home()) { get_template_part( './phps/_content-homepage' ); }
+		elseif(is_single()) { get_template_part( './phps/_content-single' ); }
+		elseif(is_page()) { get_template_part( './phps/_content-page' ); }
+	?>
+
 </div>
