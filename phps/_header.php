2ca77db68b2f4fc84ec9a305f55fdd654f5fa2a7
noisyneuron <hello@playdo.io>
2020-03-20 14:18:54 -0400

    [header] render next/prev links on post page

--- phps/_header.php
+++ phps/_header.php
@@ -1,14 +1,45 @@
 <header class="header" role="banner">
 	<nav class="nav" id="nav">
-		<button class="nav__btn -left -chevron">
+
+
+		<?php
+			$prev = "#";
+			$next = "#";
+			// if it's a post page, set $next and $prev to the URLs of next and prev posts
+			// what should happen if its a static page? ie an about page.. ?
+			if( is_single() ) {
+				if( get_adjacent_post(false, '', true) ) {
+					$prev = get_permalink(get_previous_post()->ID);
+				} else {
+					$first = new WP_Query('posts_per_page=1&order=DESC'); $first->the_post();
+					$prev = get_permalink();
+					wp_reset_query();
+				};
+
+				if( get_adjacent_post(false, '', false) ) {
+					$next = get_permalink(get_next_post()->ID);
+				} else {
+					$last = new WP_Query('posts_per_page=1&order=ASC'); $last->the_post();
+					$next = get_permalink();
+					wp_reset_query();
+				};
+			}
+		?>
+
+		<!-- changed buttons to A tags for minimal HTML changes between homepage and post page
+		just the least destructive solution for now, feel free to change. -->
+		<a href=<?php echo $prev;?> class="nav__btn -left -chevron">
 			<div class="nav__btn-chevron"></div>
-		</button>
+		</a>
+
 		<button class="nav__btn -center -ham" id="btnHam">
 			<div class="nav__btn-ham"></div>
 		</button>
-		<button class="nav__btn -right -chevron">
+
+		<a href=<?php echo $next;?> class="nav__btn -right -chevron">
 			<div class="nav__btn-chevron"></div>
-		</button>
+		</a>
+
 	</nav>
 	<div class="header__logo">
 		<a href="#" class="header__logo-link">
