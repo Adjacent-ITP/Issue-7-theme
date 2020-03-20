79e9b00c9cbc416292122679c0989b5f1b776b75
vince19972 <vince19972@gmail.com>
2020-03-20 13:39:19 -0400

    [layout] fix landing page width

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<?php get_template_part( './phps/_header' ); ?>
 	<?php get_template_part( './phps/_body' ); ?>
 
+	<?php get_footer(); ?>
+
 	<script type="text/javascript" src="<?php echo get_bloginfo('template_directory'); ?>/public/js/main.js"></script>
 </body>
-
-<?php get_footer(); ?>
