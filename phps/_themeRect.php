622688737856f434a93827e7b56baab5aa2ea6bd
vince19972 <vince19972@gmail.com>
2020-03-21 22:59:19 -0400

    [themeRect] add basic svg text

--- phps/_themeRect.php
+++ phps/_themeRect.php
@@ -1,3 +1,40 @@
 <div class="theme-rect">
-
+	<div class="theme-rect__container theme-text">
+		<svg class="theme-text__f" height="100%" width="100%">
+			<line id="f-top" x1="0" y1="0" x2="100%" y2="0" />
+			<line id="f-center" x1="0" y1="0" x2="100%" y2="0" />
+			<line id="f-left" x1="0" y1="0" x2="0" y2="100%" />
+		</svg>
+		<svg class="theme-text__e" height="100%" width="100%">
+			<line id="e-top" x1="0" y1="0" x2="100%" y2="0" />
+			<line id="e-center" x1="0" y1="0" x2="100%" y2="0" />
+			<line id="e-btm" x1="0" y1="0" x2="100%" y2="0" />
+			<line id="e-left" x1="0" y1="0" x2="0" y2="100%" />
+		</svg>
+		<svg class="theme-text__e2" height="100%" width="100%">
+			<line id="e2-top" x1="0" y1="0" x2="100%" y2="0" />
+			<line id="e2-center" x1="0" y1="0" x2="100%" y2="0" />
+			<line id="e2-btm" x1="0" y1="0" x2="100%" y2="0" />
+			<line id="e2-left" x1="0" y1="0" x2="0" y2="100%" />
+		</svg>
+		<svg class="theme-text__l" height="100%" width="100%">
+			<line id="l-btm" x1="0" y1="0" x2="100%" y2="0" />
+			<line id="l-left" x1="0" y1="0" x2="0" y2="100%" />
+		</svg>
+		<svg class="theme-text__i" height="100%" width="100%">
+			<line id="i-left" x1="0" y1="0" x2="0" y2="100%" />
+		</svg>
+		<svg class="theme-text__n" height="100%" width="100%">
+			<line id="n-left" x1="0" y1="0" x2="0" y2="100%" />
+			<line id="n-diagonal" x1="0" y1="0" x2="100%" y2="100%" />
+			<line id="n-right" x1="0" y1="0" x2="0" y2="100%" />
+		</svg>
+		<svg class="theme-text__g" height="100%" width="100%">
+			<line id="g-top" x1="0" y1="0" x2="100%" y2="0" />
+			<line id="g-left" x1="0" y1="0" x2="0" y2="100%" />
+			<line id="g-btm" x1="0" y1="0" x2="100%" y2="0" />
+			<line id="g-right" x1="0" y1="0" x2="0" y2="100%" />
+			<line id="g-center" x1="0" y1="0" x2="100%" y2="0" />
+		</svg>
+	</div>
 </div>
