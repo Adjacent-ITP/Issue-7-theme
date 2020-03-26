d5601e0ac06553ad37ac40b8a828c98116f61704
vince19972 <vince19972@gmail.com>
2020-03-26 15:17:16 -0400

    [post] build basic article text styles

--- phps/_content-single.php
+++ phps/_content-single.php
@@ -1,1 +1,64 @@
-<?php echo "~~~~~~~~~~ POST PAGE ~~~~~~~~~~" ?>
+<article class="post">
+	<div class="post__left article">
+		<div class="article__header">
+			<h1 class="article__headline -f-headline">Horror Honcho Behind The Purge and Get Out Is Hacking the Streaming Upheaval</h1>
+			<p class="article__author -f-author">Emily Lin</p>
+		</div>
+		<div class="article__intro">
+			<p class="article__blurb -f-paragraph -f-bold">
+				Media theorist Wendy Chun once differentiated the computer from other tools and formal systems through its relationship to simulation and simulated worlds. “While most tools produce effects on a wider world of which they are only a part, the computer contains its own worlds in miniature” (1).
+			</p>
+			<p class="article__illustrator -f-illustrator">
+				Illustrated by Sam Hains
+			</p>
+		</div>
+		<div class="article__main">
+
+			<p class="article__paragraph">
+				Computer worlds, like those of children’s make-believe, grant absolute power to whoever is imagining or programming them (2). As reality is drafted into digital environments, the computer simulation offers the opportunity to create worlds and bodies without unwanted complexity. It allows us to devise private games and internally consistent worlds in the face of absurdity and paradox. Advances in computer imaging technology towards “photorealism” further seduce us into believing that we are looking at concrete realities rather than ideological assemblages of math and code. As reality is increasingly reflected through the small mirrors of digital simulation, we risk losing sight of the mysterious nature of our lived reality.
+			</p>
+
+			<div class="article__quote-block">
+				<q class="article__quote">This is not an essay about technology, so much as it is about entropy.</q>
+				<span class="article__quote-author">Michelle Shevin</span>
+			</div>
+
+			<p class="article__paragraph">
+				Computer worlds, like those of children’s make-believe, grant absolute power to whoever is imagining or programming them (2). As reality is drafted into digital environments, the computer simulation offers the opportunity to create worlds and bodies without unwanted complexity. It allows us to devise private games and internally consistent worlds in the face of absurdity and paradox. Advances in computer imaging technology towards “photorealism” further seduce us into believing that we are looking at concrete realities rather than ideological assemblages of math and code. As reality is increasingly reflected through the small mirrors of digital simulation, we risk losing sight of the mysterious nature of our lived reality.
+			</p>
+
+			<span class="article__caption">Image Caption: Human unsure of life and self enjoying sunset.</span>
+
+			<p class="article__paragraph">
+				Computer worlds, like those of children’s make-believe, grant absolute power to whoever is imagining or programming them (2). As reality is drafted into digital environments, the computer simulation offers the opportunity to create worlds and bodies without unwanted complexity. It allows us to devise private games and internally consistent worlds in the face of absurdity and paradox. Advances in computer imaging technology towards “photorealism” further seduce us into believing that we are looking at concrete realities rather than ideological assemblages of math and code. As reality is increasingly reflected through the small mirrors of digital simulation, we risk losing sight of the mysterious nature of our lived reality.
+			</p>
+
+			<ul class="article__list">
+				<li class="article__list-item">black beans</li>
+				<li class="article__list-item">black beans</li>
+				<li class="article__list-item">black beans</li>
+				<li class="article__list-item">black beans</li>
+			</ul>
+
+			<hr class="article__divider">
+
+			<h2 class="article__title">III.</h2>
+
+			<p class="article__paragraph">
+				Computer worlds, like those of children’s make-believe, grant absolute power to whoever is imagining or programming them (2). As reality is drafted into digital environments, the computer simulation offers the opportunity to create worlds and bodies without unwanted complexity. It allows us to devise private games and internally consistent worlds in the face of absurdity and paradox. Advances in computer imaging technology towards “photorealism” further seduce us into believing that we are looking at concrete realities rather than ideological assemblages of math and code. As reality is increasingly reflected through the small mirrors of digital simulation, we risk losing sight of the mysterious nature of our lived reality.
+			</p>
+
+			<div class="article__footer">
+				<hr class="article__divider">
+				<p class="article__footnote-title -f-title">Footnotes</p>
+				<div class="article__footnotes">
+					<p class="article__footnote">1. Sum, Lok-kei, “Row over Extradition Bill Grows as Legco Legal Adviser Questions Proposal,” South China Morning Post, 4 May, 2019</p>
+					<p class="article__footnote">2. Sum, Lok-kei, “Row over Extradition Bill Grows as Legco Legal Adviser Questions Proposal,” South China Morning Post, 4 May, 2019</p>
+					<p class="article__footnote">3. Sum, Lok-kei, “Row over Extradition Bill Grows as Legco Legal Adviser Questions Proposal,” South China Morning Post, 4 May, 2019</p>
+				</div>
+			</div>
+
+		</div>
+	</div>
+	<div class="post__right post__gallery"></div>
+</article>
