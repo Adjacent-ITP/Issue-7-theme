3fd6c78a67b8d881f314a0a43aacb3bc570c3584
vince19972 <vince19972@gmail.com>
2020-03-26 10:25:08 -0400

    [footer] add text

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 		<footer id="site-footer" role="contentinfo" class="footer">
-			<h1>test</h1>
+			<p class="footnote__text -f-footer">ADJACENT Issue 7, published by <a target="_blank" href="https://itp.nyu.edu/">ITP &amp; IMA</a>, Tisch School of the Arts, New York University. <a href="https://www.nyu.edu/footer/accessibility.html" target="_blank">Accessibility</a> &nbsp;&nbsp;<a href="http://eepurl.com/gJqnwr" target="_blank">Subscribe to the newsletter.</a> </p>
 		</footer>
 
 </html>
