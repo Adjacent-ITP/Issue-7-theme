c36fcb9727d372110f02b71586d0bce8d8b263a6
vince19972 <vince19972@gmail.com>
2020-05-20 19:09:56 -0400

    [article] video style

--- phps/_content-single.php
+++ phps/_content-single.php
@@ -19,75 +19,15 @@
 				<?php echo get_field('blurb') ?>
 			</p>
 			<p class="article__illustrator -f-illustrator">
-				Illustrated by <?php echo get_field('illustrator') ?>
+				<?php echo get_field('illustrator') ?>
 			</p>
 		</div>
 		<div class="article__main">
 
-			<!-- <span class="article__caption" data-src="https://itp.nyu.edu/adjacent/issue-7/wp-content/uploads/sites/12/2020/05/vertical-image.jpg">1 Image Caption: Human unsure of life and self enjoying sunset.</span>
-			<figure>
-				<img src="https://itp.nyu.edu/adjacent/issue-7/wp-content/uploads/sites/12/2020/05/vertical-image.jpg" alt="1 Image Caption: Human unsure of life and self enjoying sunset.">
-				<figcaption>1 Image Caption: Human unsure of life and self enjoying sunset.</figcaption>
-			</figure>
-
-
-			<p>
-				Computer worlds, like those of children’s make-believe, grant absolute power to whoever is imagining or programming them (2). As reality is drafted into digital environments, the computer simulation offers the opportunity to create worlds and bodies without unwanted complexity. It allows us to devise private games and internally consistent worlds in the face of absurdity and paradox. Advances in computer imaging technology towards “photorealism” further seduce us into believing that we are looking at concrete realities rather than ideological assemblages of math and code. As reality is increasingly reflected through the small mirrors of digital simulation, we risk losing sight of the mysterious nature of our lived reality.
-			</p>
-
-			<div class="article__quote-block">
-				<q class="article__quote">This is not an essay about technology, so much as it is about entropy.</q>
-				<span class="article__quote-author">Michelle Shevin</span>
-			</div>
-
-			<p>
-				Computer worlds, like those of children’s make-believe, grant absolute power to whoever is imagining or programming them (2). As reality is drafted into digital environmenlts, the computer simulation offers the opportunity to create worlds and bodies without unwanted complexity. It allows us to devise private games and internally consistent worlds in the face of absurdity and paradox. Advances in computer imaging technology towards “photorealism” further seduce us into believing that we are looking at concrete realities rather than ideological assemblages of math and code. As reality is increasingly reflected through the small mirrors of digital simulation, we risk losing sight of the mysterious nature of our lived reality.
-			</p>
-
-			<span class="article__caption" data-src="https://itp.nyu.edu/adjacent/issue-7/wp-content/uploads/sites/12/2020/05/vertical-image-2-1.jpg">2 Image Caption: Human unsure of life and self enjoying sunset.</span>
-			<figure>
-				<img src="https://itp.nyu.edu/adjacent/issue-7/wp-content/uploads/sites/12/2020/05/vertical-image-2-1.jpg" alt="2 Image Caption: Human unsure of life and self enjoying sunset.">
-				<figcaption>2 Image Caption: Human unsure of life and self enjoying sunset.</figcaption>
-			</figure>
-
-			<p>
-				Computer worlds, like those of children’s make-believe, grant absolute power to whoever is imagining or programming them (2). As reality is drafted into digital environments, the computer simulation offers the opportunity to create worlds and bodies without unwanted complexity. It allows us to devise private games and internally consistent worlds in the face of absurdity and paradox. Advances in computer imaging technology towards “photorealism” further seduce us into believing that we are looking at concrete realities rather than ideological assemblages of math and code. As reality is increasingly reflected through the small mirrors of digital simulation, we risk losing sight of the mysterious nature of our lived reality.
-			</p>
-
-			<ul>
-				<li>black beans</li>
-				<li>black beans</li>
-				<li>black beans</li>
-				<li>black beans</li>
-			</ul>
-
-			<hr>
-
-			<h2>III.</h2>
-
-			<p>
-				Computer worlds, like those of children’s make-believe, grant absolute power to whoever is imagining or programming them (2). As reality is drafted into digital environments, the computer simulation offers the opportunity to create worlds and bodies without unwanted complexity. It allows us to devise private games and internally consistent worlds in the face of absurdity and paradox. Advances in computer imaging technology towards “photorealism” further seduce us into believing that we are looking at concrete realities rather than ideological assemblages of math and code. As reality is increasingly reflected through the small mirrors of digital simulation, we risk losing sight of the mysterious nature of our lived reality.
-			</p>
-
-			<span class="article__caption" data-src="https://itp.nyu.edu/adjacent/issue-7/wp-content/uploads/sites/12/2020/05/vertical-image-2.jpg">3 Image Caption: Human unsure of life and self enjoying sunset.</span>
-			<figure>
-				<img src="https://itp.nyu.edu/adjacent/issue-7/wp-content/uploads/sites/12/2020/05/vertical-image-2.jpg" alt="3 Image Caption: Human unsure of life and self enjoying sunset.">
-				<figcaption>3 Image Caption: Human unsure of life and self enjoying sunset.</figcaption>
-			</figure> -->
-
 			<?php the_content(); ?>
 
-			<div class="article__footer">
-				<hr>
-				<p class="article__footnote-title -f-title">Footnotes</p>
-				<div class="article__footnotes">
-					<?php echo get_field('footnotes'); ?>
-					<!-- <p class="article__footnote">1. Sum, Lok-kei, “Row over Extradition Bill Grows as Legco Legal Adviser Questions Proposal,” South China Morning Post, 4 May, 2019</p> -->
-				</div>
-
-				<p class="article__footnote-title -f-title"><?php echo get_field('author_name') ?></p>
-				<p><?php echo get_field('author_bio') ?></p>
-			</div>
+			<p class="article__footnote-title"><?php echo get_field('author_name') ?></p>
+			<p><?php echo get_field('author_bio') ?></p>
 
 		</div>
 		</div>
