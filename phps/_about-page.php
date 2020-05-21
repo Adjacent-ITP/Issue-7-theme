f673f5c2f2d72f9a5d0704b92633d28bb3e627ee
vince19972 <vince19972@gmail.com>
2020-05-21 12:23:02 -0400

    [about] add basic page layout

---
+++ phps/_about-page.php
@@ -0,0 +1,73 @@
+<div class="about">
+	<div class="about__row">
+		<img src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/logo.svg" alt="logo" class="about__logo">
+	</div>
+	<div class="about__row">
+		<p class="about__paragraph -f-bold">
+		ADJACENT is an online journal of emerging media published by the Interactive Telecommunications Program, or ITP. Its mission is to share research, reflection, observations, and opinions from and for the diverse creators that are exploring the possibilities and directions at the frontiers of media and technology.
+		</p>
+		<p class="about__paragraph">
+		Old/New/Next sounds like it could be a slogan of mid-20th century America: casting off the past, ascendantly modern, moving toward a bright future. But in a contemporary moment in which so much of the future feels as though it has already been predicted, data mined, and sold off, this slogan takes on a more ironic tone, and taking the time to cast a critical eye back toward the past seems like an act of resilience, or at least an act of refuge-seeking. The Interactive Telecommunications Program is turning 40 this year, and for Adjacent Issue 6 we wanted to take this birthday as an opportunity for us to look back on the past 40 years of media and technology, reflect on what that history can inform us about the future of the field, and interpret how the dreams of the past brought about the present, for better and for worse.
+		</p>
+	</div>
+	<div class="about__row">
+		<div class="about__team">
+			<h2 class="about__team-title">Editorial Board</h2>
+			<ul class="about__team-list">
+				<li class="about__team-list-item">Nancy Hechinger, Editor-in-Chief</li>
+				<li class="about__team-list-item">Alden Rivendale Jones & Carrie Wang, Managing Editors</li>
+			</ul>
+		</div>
+		<div class="about__team">
+			<h2 class="about__team-title">Editors</h2>
+			<ul class="about__team-list">
+				<li class="about__team-list-item">Gabriella M Garcia, Senior Editor</li>
+				<li class="about__team-list-item">Na Chen</li>
+				<li class="about__team-list-item">Daniel Fries</li>
+				<li class="about__team-list-item">Karina Hyland</li>
+				<li class="about__team-list-item">Ashley Jane Lewis</li>
+				<li class="about__team-list-item">Melissa Powers</li>
+				<li class="about__team-list-item">Lillian Ritchie</li>
+				<li class="about__team-list-item">Matthew Ross			</li>
+			</ul>
+		</div>
+		<div class="about__team">
+			<h2 class="about__team-title">Art Direction: Site Design</h2>
+			<ul class="about__team-list">
+				<li class="about__team-list-item">Dana Elkis</li>
+				<li class="about__team-list-item">Emily Lin</li>
+				<li class="about__team-list-item">Ilana Bonder			</li>
+			</ul>
+		</div>
+		<div class="about__team">
+			<h2 class="about__team-title">Art Director: Visuals/Illustration</h2>
+			<ul class="about__team-list">
+				<li class="about__team-list-item">Nick Gregg</li>
+			</ul>
+		</div>
+		<div class="about__team">
+			<h2 class="about__team-title">Web Development</h2>
+			<ul class="about__team-list">
+				<li class="about__team-list-item">Sukanya Aneja</li>
+				<li class="about__team-list-item">Vince Mingpu Shao			</li>
+			</ul>
+		</div>
+		<div class="about__team">
+			<h2 class="about__team-title">Communications and Public Relations</h2>
+			<ul class="about__team-list">
+				<li class="about__team-list-item">Gabriella M Garcia, Director</li>
+				<li class="about__team-list-item">Sarah Liriano</li>
+				<li class="about__team-list-item">Yiting Liu</li>
+				<li class="about__team-list-item">Marcela Mancino</li>
+				<li class="about__team-list-item">Kat Vlasova			</li>
+			</ul>
+			<p class="about__team-paragraph">ADJACENT was made possible by a Tisch Faculty Grant. Special thanks to Tisch School of the Arts Dean, Allyson Green, and ITP’s Chair and the Associate Dean for Emerging Media, Dan O’Sullivan, for their ongoing support.</p>
+		</div>
+		<div class="about__team">
+			<h2 class="about__team-title">Contact</h2>
+			<ul class="about__team-list">
+				<li class="about__team-list-item">adjacent@itp.nyu.eduv</li>
+			</ul>
+		</div>
+	</div>
+</div>
--- phps/_contentArea.php
+++ phps/_contentArea.php
@@ -4,8 +4,11 @@
 
 	<div class="content-area__main">
 	<?php
+		$slug = get_post_field( 'post_name', get_post() );
+
 		if(is_home()) { get_template_part( './phps/_content-homepage' ); }
 		elseif(is_single()) { get_template_part( './phps/_content-single' ); }
+		elseif($slug == "about") { get_template_part( './phps/_about-page' ); }
 		elseif(is_page()) { get_template_part( './phps/_content-page' ); }
 	?>
 	</div>
