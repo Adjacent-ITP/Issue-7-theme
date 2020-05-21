211ede5bbb67361ebfbb9eeeff6bd7ab46a15a77
vince19972 <vince19972@gmail.com>
2020-05-21 12:23:24 -0400

    [menu] update post fetch php fn

--- phps/_menu.php
+++ phps/_menu.php
@@ -1,50 +1,43 @@
 <menu class="menu" id="menu">
 	<div class="menu__row -big -top">
-	  <?php $count = 0; ?>
-		<?php while (have_posts()) : ?>
-			<?php the_post(); $count++; ?>
-			<div class="menu__cell">
-				<a href="<?php the_permalink(); ?>" class="menu__cell-post menu-post">
-					<img
-						src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/number_<?php echo $count;?>.svg"
-						alt="<?php echo $count;?>"
-						class="menu-post__number"
-					>
-					<div class="menu-post__text">
-						<p class="menu-post__title"><?php the_title(); ?></p>
-						<p class="menu-post__author">By <?php the_field('author_name') ?></p>
-					</div>
-				</a>
-			</div>
-			<?php if($count == 3): ?>
-			<div class="menu__cell -small">
-				<a href="#" class="menu__cell-post menu-post -small">
-					<p class="menu-post__title">About</p>
-				</a>
-			</div>
-			<div class="menu__cell -small">
-				<a href="#" class="menu__cell-post menu-post -small">
-					<p class="menu-post__title">Previous Issues</p>
-				</a>
-			</div>
-			<div class="menu__cell -small">
-				<a href="#" class="menu__cell-post menu-post -small">
-					<p class="menu-post__title">Submit</p>
-				</a>
-			</div>
-			<?php endif; ?>
-  	<?php endwhile; ?>
-		<!-- <div class="menu__cell"></div>
-		<div class="menu__cell"></div> -->
+		<?php
+			$all_posts = get_posts( array('posts_per_page' => 20) );
+			setup_postdata( $post );
+		?>
+			<?php foreach ($all_posts as $key=>$post) : ?>
+				<div class="menu__cell">
+					<a href="<?php the_permalink(); ?>" class="menu__cell-post menu-post">
+						<img
+							src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/number_<?php echo ($key+1);?>.svg"
+							alt="<?php echo ($key+1);?>"
+							class="menu-post__number"
+						>
+						<div class="menu-post__text">
+							<p class="menu-post__title"><?php the_title(); ?></p>
+							<p class="menu-post__author">By <?php the_field('author_name') ?></p>
+						</div>
+					</a>
+				</div>
+				<?php if($key == 2): ?>
+				<div class="menu__cell -small">
+					<a href="<?php echo get_bloginfo('url'); ?>/about" class="menu__cell-post menu-post -small">
+						<p class="menu-post__title">About</p>
+					</a>
+				</div>
+				<div class="menu__cell -small">
+					<a href="https://itp.nyu.edu/adjacent" target="_blank" class="menu__cell-post menu-post -small">
+						<p class="menu-post__title">Previous Issues</p>
+					</a>
+				</div>
+				<div class="menu__cell -small">
+					<a href="https://forms.gle/YH3T8HjcfbqTVoMy5" target="_blank" class="menu__cell-post menu-post -small">
+						<p class="menu-post__title">Submit</p>
+					</a>
+				</div>
+				<?php endif; ?>
+			<?php
+				endforeach;
+				wp_reset_postdata();
+			?>
 	</div>
-	<!-- <div class="menu__row -small -center">
-		<div class="menu__cell"></div>
-		<div class="menu__cell"></div>
-		<div class="menu__cell"></div>
-	</div>
-	<div class="menu__row -big -btm">
-		<div class="menu__cell"></div>
-		<div class="menu__cell"></div>
-		<div class="menu__cell"></div>
-	</div> -->
 </menu>
