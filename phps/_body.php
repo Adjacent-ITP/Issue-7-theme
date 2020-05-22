0f4618f3deca44e612f0a28f967f60ad73bf513f
vince19972 <vince19972@gmail.com>
2020-05-21 20:18:55 -0400

    [rwd] basic post page rwd style

--- phps/_body.php
+++ phps/_body.php
@@ -1,4 +1,4 @@
-<div class="main -flex">
+<div class="main -flex <?php if(is_single()) echo "-is-post"; ?>">
 	<?php get_template_part( './phps/_contentArea' ); ?>
 	<?php get_template_part( './phps/_themeRect' ); ?>
 </div>
