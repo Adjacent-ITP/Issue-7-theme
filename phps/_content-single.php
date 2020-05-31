5aa62717741e432f10bc40ac22988803d1e7106c
noisyneuron <hello@playdo.io>
2020-05-31 12:32:54 -0400

    [embeds] updated vimeo embed codes to be bg elements

--- phps/_content-single.php
+++ phps/_content-single.php
@@ -22,7 +22,7 @@
 
 		<?php if(get_field('layout') == 'iframe'): ?>
 			<div class="video-holder -is-iframe-layout">
-				<div style="padding:176.94% 0 0 0;position:relative;"><iframe src="<?php the_field('side_video'); ?>?autoplay=1&loop=1" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+				<div style="padding:176.94% 0 0 0;position:relative;"><iframe src="<?php the_field('side_video'); ?>?background=1" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 			</div>
 		<?php endif; ?>
 
@@ -39,18 +39,18 @@
 			<div class="gallery__wrapper" id="galleryImg"></div>
 		<?php elseif(get_field('layout') == "iframe"): ?>
 			<div class="gallery__wrapper">
-				<div style="padding:176.94% 0 0 0;position:relative;"><iframe src="<?php the_field('side_video'); ?>?autoplay=1&loop=1" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+				<div style="padding:176.94% 0 0 0;position:relative;"><iframe src="<?php the_field('side_video'); ?>?background=1" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 			</div>
 		<?php else: ?>
 			<div class="gallery__wrapper -is-iframes" id="galleryWrapper">
 				<div class="gallery__wrapper-video -is-active">
-					<div><iframe src="https://player.vimeo.com/video/423278597?autoplay=1&loop=1" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div>
+					<div><iframe src="https://player.vimeo.com/video/423278597?background=1" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div>
 				</div>
 				<div class="gallery__wrapper-video">
-					<div><iframe src="https://player.vimeo.com/video/423281620?autoplay=1&loop=1" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div>
+					<div><iframe src="https://player.vimeo.com/video/423281620?background=1" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div>
 				</div>
 				<div class="gallery__wrapper-video">
-					<div><iframe src="https://player.vimeo.com/video/423283602?autoplay=1&loop=1" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div>
+					<div><iframe src="https://player.vimeo.com/video/423283602?background=1" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div>
 				</div>
 			</div>
 		<?php endif; ?>
